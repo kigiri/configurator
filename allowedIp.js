0f95a67a3a669e017618867a6ee09ca109b26046
Clément Denis <le.mikmac@gmail.com>
2017-05-08 09:43:19 +0200

    fixezz

--- allowedIp.js
+++ allowedIp.js
@@ -1,6 +1,10 @@
-module.exports = new Set([
-  "::ffff:127.0.0.1",
+module.exports = [
+  "127.0.0.1",
   "89.84.91.68",  // aos
   "78.224.65.10", // spam
   "31.38.18.133", // thai
-])
+].reduce((set, ip) => {
+  set.add(`::ffff:${ip}`)
+  set.add(ip)
+  return set
+}, new Set())
