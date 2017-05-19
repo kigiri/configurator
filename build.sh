727c72882c06d4d140378c0ac10437d8bff1f022
Clement Denis <le.mikmac@gmail.com>
2017-05-19 16:20:06 +0200

    add raw sql exec

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ./node_modules/.bin/tupac --entry="./db.js" --build="db.html" --title="Chupato DB"
+./node_modules/.bin/tupac --entry="./exec.js" --build="exec.html" --title="Chupato Exec SQL"
 ./node_modules/.bin/tupac --entry="./controltower.js" --build="controltower.html" --title="CONTROL TOWER"
 ./node_modules/.bin/tupac --build --title="Chupato Config"
